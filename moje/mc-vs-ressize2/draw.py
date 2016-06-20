01473b3ecce4486e92d6fcbd7d82938536d86725
pe-ge <gergelp@gmail.com>
2016-06-20 21:33:44 +0200

    Continuous heatmap

--- moje/mc-vs-ressize2/draw.py
+++ moje/mc-vs-ressize2/draw.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-data = np.array(pickle.load(open('collect-rhos/data3.pickle', 'rb')))
+data = np.array(pickle.load(open('collect-rhos/data0.pickle', 'rb')))
 
 x = np.linspace(0.8, 1.10, 20)
 lines = data[5, :, :].transpose()
