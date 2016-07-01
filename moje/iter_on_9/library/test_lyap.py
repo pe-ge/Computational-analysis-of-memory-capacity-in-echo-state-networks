f9e0e9f5ce6cb6daa0498662fc8418d615fa66a0
pe-ge <gergelp@gmail.com>
2016-07-01 22:10:38 +0200

    fixed library

--- moje/iter_on_9/library/test_lyap.py
+++
@@ -1,7 +0,0 @@
-from lyapunov import lyapunov_exp
-import numpy as np
-
-W = np.loadtxt('../lyapunov/W')
-Win = np.loadtxt('../lyapunov/Win')
-x = np.loadtxt('../lyapunov/x')
-print(lyapunov_exp(W, Win, x))
