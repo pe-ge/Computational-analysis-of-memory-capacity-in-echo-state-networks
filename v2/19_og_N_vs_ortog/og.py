a2404386cdf480f96e6bcb750077a54e029bb137
pe-ge <gergelp@gmail.com>
2017-03-20 12:41:35 +0100

    N vs ortog using rho instead of sigma

---
+++ v2/19_og_N_vs_ortog/og.py
@@ -0,0 +1,43 @@
+from library.ortho import learn_orthogonal
+# from library.ortho import learn_orthonormal
+from library.ortho import orthogonality
+
+import numpy as np
+from numpy import random
+
+
+INSTANCES = 2
+ORTHOPROCESS_ITERATIONS = 50
+
+rho = 0.95
+tau = 10**-10
+# eta_0 = 7*10**-2
+eta = 3*10**-2
+
+reservoir_sizes = list(range(100, 1000 + 1, 100))
+
+ortog_mean = np.zeros((len(reservoir_sizes), ORTHOPROCESS_ITERATIONS + 1))
+ortog_std = np.zeros((len(reservoir_sizes), ORTHOPROCESS_ITERATIONS + 1))
+
+for rsi, N in enumerate(reservoir_sizes):
+    print(N)
+    instances = np.zeros((INSTANCES, ORTHOPROCESS_ITERATIONS + 1))
+    for inst in range(INSTANCES):
+        W = np.random.normal(0, 1, [N, N])
+        W = W * (rho / np.max(np.abs(np.linalg.eig(W)[0])))
+        WI = random.uniform(-tau, tau, N)
+
+        instances[inst, 0] = orthogonality(W)
+
+        # eta = eta_0
+        for it in range(ORTHOPROCESS_ITERATIONS):
+            W = learn_orthogonal(W, eta)
+            # W = learn_orthonormal(W, eta)
+            instances[inst, it + 1] = orthogonality(W)
+            # eta = eta * 0.9
+
+    ortog_mean[rsi, :] = np.mean(instances, axis=0)
+    ortog_std[rsi, :] = np.std(instances, axis=0)
+
+    np.save('ortog_mean', ortog_mean)
+    np.save('ortog_std', ortog_std)
---
+++ v2/19_og_N_vs_ortog/plot.py
@@ -0,0 +1,22 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from library.aux import try_save_fig
+
+means = np.load('ortog_mean.npy')
+stds = np.load('ortog_std.npy')
+
+reservoir_sizes = list(range(100, 1000 + 1, 100))
+ORTHOPROCESS_ITERATIONS = 50
+iterations = range(ORTHOPROCESS_ITERATIONS + 1)
+
+for i in range(means.shape[0]):
+    plt.errorbar(iterations, means[i, :], yerr=stds[i, :], label=reservoir_sizes[i])
+
+plt.xlabel('iterations')
+plt.ylabel('orthogonality')
+plt.title('OG method')
+# plt.grid(True)
+plt.legend(loc=4)
+try_save_fig("figures/figure")
+try_save_fig("figures/figure", ext="pdf")
+plt.show()
