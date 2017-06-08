bb596ccb21e0cd81ccf86d6533e6bf12118958d4
pe-ge <gergelp@gmail.com>
2017-06-08 21:50:28 +0200

    refactored computation of mc

--- v3/1_og_cn/og.py
+++ v3/1_og_cn/og.py
@@ -3,36 +3,117 @@ from library.ortho import learn_orthogonal
 import numpy as np
 from numpy import random
 from numpy.linalg import cond
+from scipy.linalg import pinv
 
 INSTANCES = 5
-ORTHOPROCESS_ITERATIONS = 50
+ORTHO_ITERS = 50
+
 RHO = 0.95
 TAU = 10**-10
+
+
 # ETA_0 = 7*10**-2
 ETA = 3*10**-2
 
-reservoir_sizes = list(range(100, 1000 + 1, 100))
-res_sizes = len(reservoir_sizes)
+RES_SIZES = list(range(100, 1000 + 1, 100))
 
-cond_before = np.zeros((res_sizes, INSTANCES))
-cond_after = np.zeros((res_sizes, INSTANCES))
+cond_before = np.zeros((len(RES_SIZES), INSTANCES))
+cond_after = np.zeros((len(RES_SIZES), INSTANCES))
 
-for N_idx, N in enumerate(reservoir_sizes):
+
+def mc(WI, W, iters_skip, iters_train, iters_test):
+    # num output neurons
+    N = WI.shape[0]
+    L = N
+    total_its = iters_skip + iters_train + iters_test
+
+    # input
+    u = np.random.uniform(-1.0, 1.0, total_its)
+    # desired outputs
+    D = np.zeros([L, iters_train])
+    # reservoir activations
+    X = np.zeros(N)
+    # history of reservoir activations
+    Xs = np.zeros([N, iters_train])
+
+    # skipping (getting rid of transients)
+    for it in range(iters_skip):
+        X = np.tanh(np.dot(W, X) + np.dot(WI, u[it]))
+
+    # training
+    for it in range(iters_train):
+        X = np.tanh(np.dot(W, X) + np.dot(WI, u[iters_skip + it]))
+        Xs[:, it] = X
+        D[:, it] = u[iters_skip + it:it:-1]
+
+    # calculate output weights
+    Xs_pinv = pinv(Xs)
+    WO = np.dot(D, Xs_pinv)
+
+    # testing
+    y = np.zeros([L, iters_test])
+    for it in range(iters_test):
+        X = np.tanh(np.dot(W, X) + np.dot(WI, u[iters_skip + iters_train + it]))
+        y[:, it] = np.dot(WO, X)
+
+    # memory capacity
+    mc = np.zeros(L)
+    for h in range(L):
+        cc = np.corrcoef(u[iters_skip + iters_train - h: total_its - h], y[h, :])[0, 1]
+        mc[h] = cc * cc
+
+    return np.sum(mc)
+
+
+def pinv_acts(WI, W, iters_skip, iters_train, iters_test):
+    # num output neurons
+    N = WI.shape[0]
+    L = N
+    total_its = iters_skip + iters_train + iters_test
+
+    # input
+    u = np.random.uniform(-1.0, 1.0, total_its)
+    # desired outputs
+    D = np.zeros([L, iters_train])
+    # reservoir activations
+    X = np.zeros(N)
+    # history of reservoir activations
+    Xs = np.zeros([N, iters_train])
+
+    # skipping (getting rid of transients)
+    for it in range(iters_skip):
+        X = np.tanh(np.dot(W, X) + np.dot(WI, u[it]))
+
+    # training
+    for it in range(iters_train):
+        X = np.tanh(np.dot(W, X) + np.dot(WI, u[iters_skip + it]))
+        Xs[:, it] = X
+        D[:, it] = u[iters_skip + it:it:-1]
+
+    # calculate output weights
+    return pinv(Xs)
+
+
+for N_idx, N in enumerate(RES_SIZES):
     print(N)
     for inst_idx in range(INSTANCES):
         W = np.random.normal(0, 1, [N, N])
         W = W * (RHO / np.max(np.abs(np.linalg.eig(W)[0])))
         WI = random.uniform(-TAU, TAU, N)
 
-        cond_before[N_idx, inst_idx] = cond(W)
+        cond_before[N_idx, inst_idx] = cond(pinv_acts(WI, W, iters_skip=N, iters_train=10*N, iters_test=1000))
 
         # eta = ETA_0
         eta = ETA
-        for it in range(ORTHOPROCESS_ITERATIONS):
+        for it in range(ORTHO_ITERS):
             W = learn_orthogonal(W, eta)
+            # W = learn_orthonormal(W, eta)
             # eta = eta * 0.9
 
-        cond_after[N_idx, inst_idx] = cond(W)
+        cond_after[N_idx, inst_idx] = cond(pinv_acts(WI, W, iters_skip=N, iters_train=10*N, iters_test=1000))
+
+        print('before: {}'.format(cond_before[N_idx, inst_idx]))
+        print('after: {}'.format(cond_after[N_idx, inst_idx]))
 
     np.save('cond_before', cond_before)
     np.save('cond_after', cond_after)
