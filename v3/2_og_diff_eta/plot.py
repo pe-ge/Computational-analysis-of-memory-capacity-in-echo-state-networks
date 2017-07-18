dde21d95b0f6334bec4cf2c4ef78d24ef470f321
pe-ge <gergelp@gmail.com>
2017-07-18 21:25:14 +0200

    fixed plot

--- v3/2_og_diff_eta/plot.py
+++ v3/2_og_diff_eta/plot.py
@@ -5,11 +5,12 @@ smoothness = 20
 etas = np.linspace(0.005, 0.1, smoothness)
 
 data = np.load('mc.npy')
+print(np.max(data, axis=0))
 # print(data)
 plt.xlabel(r'$\eta$')
 plt.ylabel('MC')
 plt.title('OG method, 10 instances, 20 iterations each')
 plt.errorbar(etas, np.mean(data, axis=1), np.std(data, axis=1))
 
-plt.show()
 plt.savefig('OG.png')
+plt.show()
--- v3/2_on_diff_eta/plot.py
+++ v3/2_on_diff_eta/plot.py
@@ -3,21 +3,27 @@ import matplotlib.pyplot as plt
 import matplotlib
 from library.aux import try_save_fig
 
-data = np.load('mc50.npy')
+data = np.load('mc.npy')
+# print(np.max(data, axis=2))
 heatmap = np.mean(data, axis=2)
 
 # eta_min = 0.1
 # eta_max = 0.5
 # xi_min = 0.
 # xi_max = 1
-eta_min = 0.005
-eta_max = 0.1
-xi_min = 0.85
-xi_max = 1
+smoothness = 20
+etas = np.linspace(0.005, 0.1, smoothness)
+xis = np.linspace(0.9, 1.0, smoothness)
+
+eta_min = np.min(etas)
+eta_max = np.max(etas)
+xi_min = np.min(xis)
+xi_max = np.max(xis)
 
 fig = plt.figure(figsize=(8, 6.5))
 plt.xlabel(r'$\eta_0$', size=24)
 plt.ylabel(r'$\xi$', size=24)
+plt.title('ON method, 10 instances, 10 iterations each')
 
 plt.imshow(heatmap,
            origin='lower',
@@ -25,10 +31,11 @@ plt.imshow(heatmap,
            extent=[eta_min, eta_max, xi_min, xi_max],
            aspect='auto')
 
-plt.colorbar(label='memory capacity', ticks=[40, 50, 60, 70, 80, 90, 100])
+# plt.colorbar(label='memory capacity', ticks=[40, 50, 60, 70, 80, 90, 100])
 matplotlib.rcParams.update({'font.size': 18})
-# plt.colorbar()
+plt.colorbar()
 
-try_save_fig('eta_xi_mc.png', ext="pdf")
+# try_save_fig('eta_xi_mc.png', ext="pdf")
+plt.savefig('ON.png')
 
 plt.show()
